b2ade4763b4cb3cd974a77da5205ff0143245a7c
ireneguerrero <irene2g222@gmail.com>
2023-05-29 13:57:58 +0200

    a

--- src/main/java/interfaces/PantallaJugador.java
+++ src/main/java/interfaces/PantallaJugador.java
@@ -1,7 +1,79 @@
 package interfaces;
 
 import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class PantallaJugador extends JPanel {
-
+	private Ventana ventana;
+	private JTextField textNombre;
+	private JPasswordField passwordField;
+	
+	public PantallaJugador(Ventana v) {
+		this.ventana=v;
+		setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("Labyrinth");
+		lblTitulo.setBounds(282, 30, 135, 36);
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setForeground(new Color(255, 255, 255));
+		lblTitulo.setFont(new Font("Ink Free", Font.BOLD | Font.ITALIC, 30));
+		add(lblTitulo);
+		
+		JLabel lblUsuario = new JLabel("Nombre");
+		lblUsuario.setBounds(314, 133, 62, 21);
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setFont(new Font("Franklin Gothic Book", Font.ITALIC, 18));
+		lblUsuario.setForeground(new Color(255, 255, 255));
+		add(lblUsuario);
+		
+		textNombre = new JTextField();
+		textNombre.setBounds(254, 185, 185, 19);
+		add(textNombre);
+		textNombre.setColumns(10);
+		
+		JLabel lblContraseña = new JLabel("Contraseña");
+		lblContraseña.setBounds(301, 242, 90, 21);
+		lblContraseña.setHorizontalAlignment(SwingConstants.CENTER);
+		lblContraseña.setForeground(Color.WHITE);
+		lblContraseña.setFont(new Font("Franklin Gothic Book", Font.ITALIC, 18));
+		add(lblContraseña);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(254, 287, 185, 19);
+		add(passwordField);
+		
+		JButton btnNewButton = new JButton("Empezar");
+		btnNewButton.setBounds(282, 371, 135, 29);
+		btnNewButton.setFont(new Font("Franklin Gothic Book", Font.ITALIC, 18));
+		add(btnNewButton);
+		
+		JLabel lblFondo = new JLabel("");
+		try {
+			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo1.png"));
+			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
+			lblFondo.setIcon(new ImageIcon(enIcono));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		lblFondo.setBounds(0, 0, 700, 500);
+		add(lblFondo);
+		
+	}
 }
