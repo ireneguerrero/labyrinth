c27f13bbd6c5a13e2d3c625e0fe62b0b156d08f1
ireneguerrero <irene2g222@gmail.com>
2023-05-30 20:05:48 +0200

    a

--- src/main/java/interfaces/Lvl2.java
+++ src/main/java/interfaces/Lvl2.java
@@ -1,19 +1,23 @@
 package interfaces;
 
 import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Lvl2 extends JPanel {
 	private Ventana ventana;
@@ -22,29 +26,7 @@ public class Lvl2 extends JPanel {
 		this.ventana = v;
 		setLayout(null);
 
-		JButton btnSiguiente = new JButton("Siguiente nivel");
-		btnSiguiente.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ventana.cambiarAPantalla(Lvl3.class);
-			}
-		});
-		btnSiguiente.setBounds(475, 385, 155, 45);
-		btnSiguiente.setForeground(new Color(230, 0, 0));
-		btnSiguiente.setFont(new Font("Franklin Gothic Book", Font.BOLD | Font.ITALIC, 18));
-		btnSiguiente.setBackground(new Color(255, 215, 0));
-		add(btnSiguiente);
-
-		JLabel lblFondo = new JLabel("");
-		try {
-			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_katakuri.jpg"));
-			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
-			lblFondo.setIcon(new ImageIcon(enIcono));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		lblFondo.setBounds(0, 0, 700, 500);
-		add(lblFondo);
+		
+		
 	}
-
 }
--- src/main/java/interfaces/Lvl4.java
+++ src/main/java/interfaces/Lvl4.java
@@ -1,49 +1,32 @@
 package interfaces;
 
-import java.awt.Color;
-import java.awt.Font;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Lvl4 extends JPanel {
 	private Ventana ventana;
+
 	public Lvl4(Ventana v) {
-		this.ventana=v;
+		this.ventana = v;
 		setLayout(null);
+
 		
-		JButton btnFin = new JButton("Fin");
-		btnFin.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ventana.cambiarAPantalla(FinJuego.class);
-			}
-		});
-		btnFin.setBounds(475, 385, 155, 45);
-		btnFin.setForeground(new Color(230, 0, 0));
-		btnFin.setFont(new Font("Franklin Gothic Book", Font.BOLD | Font.ITALIC, 18));
-		btnFin.setBackground(new Color(255, 215, 0));
-		add(btnFin);
-
-		JLabel lblFondo = new JLabel("");
-		try {
-			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_kaido.jpg"));
-			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
-			lblFondo.setIcon(new ImageIcon(enIcono));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		lblFondo.setBounds(0, 0, 700, 500);
-		add(lblFondo);
+		
 	}
-
 }
