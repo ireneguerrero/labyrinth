51fbf989e6a42c88ed0f701eb800c7395df0429e
ireneguerrero <irene2g222@gmail.com>
2023-06-01 13:33:10 +0200

    a

--- src/main/java/interfaces/BossLvl1.java
+++ src/main/java/interfaces/BossLvl1.java
@@ -17,6 +17,7 @@ import java.awt.event.MouseEvent;
 
 public class BossLvl1 extends JPanel {
 	private Ventana ventana;
+	private JLabel labelImagen;
 
 	public BossLvl1(Ventana v) {
 		this.ventana = v;
@@ -35,24 +36,33 @@ public class BossLvl1 extends JPanel {
 		btnSiguiente.setFont(new Font("Franklin Gothic Book", Font.BOLD | Font.ITALIC, 18));
 		add(btnSiguiente);
 		
-		JLabel lblFondo = new JLabel("");
-		try {
-			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_doflamingo.jpg"));
-			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
-			lblFondo.setIcon(new ImageIcon(enIcono));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		lblFondo.setBounds(0, 0, 700, 500);
-		add(lblFondo);
+//		JLabel lblFondo = new JLabel("");
+//		try {
+//			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_doflamingo.jpg"));
+//			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
+//			lblFondo.setIcon(new ImageIcon(enIcono));
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+//		lblFondo.setBounds(0, 0, 700, 500);
+//		add(lblFondo);
 		
 		//TODO: cambiar los tamaños de los iconos
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(BossLvl1.class.getResource("/iconos/luffy.png")));
-//		Image enIcono = imagen.getScaledInstance(200, 100, Image.SCALE_SMOOTH);
-		lblNewLabel.setBounds(44, 104, 196, 307);
-		add(lblNewLabel);
+		
+		labelImagen = new JLabel("");
+		// para escalar las imágenes al tamaño que queramos
+		try {
+			BufferedImage imagen = ImageIO.read(new File("./iconos/luffy.png"));
+			Image enIcono = imagen.getScaledInstance(200, 100, Image.SCALE_SMOOTH);
+			labelImagen.setIcon(new ImageIcon(enIcono));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		labelImagen.setBounds(10, 156, 223, 274);
+		add(labelImagen);
+		
+		
 		
 		
 	}
