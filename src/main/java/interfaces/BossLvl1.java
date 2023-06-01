b63321ffc57f04ccab1dbd274f4f3e3a306ab15b
ireneguerrero <irene2g222@gmail.com>
2023-06-01 14:05:05 +0200

    Update BossLvl1.java

--- src/main/java/interfaces/BossLvl1.java
+++ src/main/java/interfaces/BossLvl1.java
@@ -17,7 +17,6 @@ import java.awt.event.MouseEvent;
 
 public class BossLvl1 extends JPanel {
 	private Ventana ventana;
-	private JLabel labelImagen;
 
 	public BossLvl1(Ventana v) {
 		this.ventana = v;
@@ -36,31 +35,31 @@ public class BossLvl1 extends JPanel {
 		btnSiguiente.setFont(new Font("Franklin Gothic Book", Font.BOLD | Font.ITALIC, 18));
 		add(btnSiguiente);
 		
-//		JLabel lblFondo = new JLabel("");
-//		try {
-//			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_doflamingo.jpg"));
-//			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
-//			lblFondo.setIcon(new ImageIcon(enIcono));
-//		} catch (IOException e1) {
-//			e1.printStackTrace();
-//		}
-//		lblFondo.setBounds(0, 0, 700, 500);
-//		add(lblFondo);
+		JLabel lblLuffy = new JLabel("");
+		lblLuffy.setIcon(new ImageIcon(BossLvl1.class.getResource("/iconos/luffy.png")));
+		lblLuffy.setBounds(31, 168, 241, 300);
+		add(lblLuffy);
+		
+		JLabel lblDoflamingo = new JLabel("");
+		lblDoflamingo.setIcon(new ImageIcon(BossLvl1.class.getResource("/iconos/doflamingo.png")));
+		lblDoflamingo.setBounds(421, 10, 269, 300);
+		add(lblDoflamingo);
+		
+		JLabel lblFondo = new JLabel("");
+		try {
+			BufferedImage imagen = ImageIO.read(new File("./imagenes/fondo_luffy_doflamingo.jpg"));
+			Image enIcono = imagen.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
+			lblFondo.setIcon(new ImageIcon(enIcono));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		lblFondo.setBounds(0, 0, 700, 500);
+		add(lblFondo);
 		
 		//TODO: cambiar los tamaños de los iconos
 		
 		
-		labelImagen = new JLabel("");
-		// para escalar las imágenes al tamaño que queramos
-		try {
-			BufferedImage imagen = ImageIO.read(new File("./iconos/luffy.png"));
-			Image enIcono = imagen.getScaledInstance(200, 100, Image.SCALE_SMOOTH);
-			labelImagen.setIcon(new ImageIcon(enIcono));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		labelImagen.setBounds(10, 156, 223, 274);
-		add(labelImagen);
+		
 		
 		
 		
